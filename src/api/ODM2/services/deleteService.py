ab94e6d34cd0aa210290a9492886277808eadf1e
farrieta9 <francisco.arrieta@aggiemail.usu.edu>
2015-08-20 15:50:15 -0600

    Worked on the deleteService.

--- src/api/ODM2/services/deleteService.py
+++ src/api/ODM2/services/deleteService.py
@@ -1,7 +1,8 @@
 __author__ = 'jmeline'
 
 from .. import serviceBase
-from ...ODM2.models import *
+# from ...ODM2.models import *
+from ODM2PythonAPI.src.api.ODM2 import models
 
 # ################################################################################
 # Annotations
@@ -11,6 +12,67 @@ class DeleteODM2(serviceBase):
 
     def test(self):
         return None
+
+    def deleteActionBy_ByID(self, id):
+        try:
+            self._session.query(models.ActionBy).filter_by(ActionID=id).delete()
+            self._session.commit()
+        except:
+            return None
+
+    def deleteActionByID(self, id):
+        try:
+            self._session.query(models.Actions).filter_by(ActionID=id).delete()
+            self._session.commit()
+        except:
+            return None
+
+    def deleteAffiliationByID(self, id):
+        try:
+            self._session.query(models.Affiliations).filter_by(AffiliationID=id).delete()
+            self._session.commit()
+        except:
+            return None
+
+    def deleteModelByID(self, id):
+        try:
+            # self._session.query(models.Models).delete()
+            self._session.query(models.Models).filter_by(ModelID=id).delete()
+            self._session.commit()
+        except:
+            return None
+
+    def deleteOrganizationByID(self, id):
+        try:
+            self._session.query(models.Organizations).filter_by(OrganizationID=id).delete()
+        except:
+            return None
+
+    def deletePeopleByID(self, id):
+        try:
+            self._session.query(models.People).filter_by(PersonID=id).delete()
+            self._session.commit()
+        except:
+            return None
+
+    def deleteSimulationByID(self, id):
+        try:
+            self._session.query(models.Simulations).filter_by(SimulationID=id).delete()
+            self._session.commit()
+        except:
+            return None
+
+    def deleteUnitByID(self, id):
+        try:
+            self._session.query(models.Units).filter_by(UnitsID=id).delete()
+            self._session.commit()
+        except:
+            return None
+
+
+
+
+
 
 # ################################################################################
 # CV
